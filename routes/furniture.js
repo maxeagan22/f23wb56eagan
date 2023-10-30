48550bda2dbeeb78b18d7dbd643257ae2d71b229
maxeagan22 <124809445+maxeagan22@users.noreply.github.com>
2023-10-30 18:31:42 -0500

    Update furniture.js

--- routes/furniture.js
+++ routes/furniture.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/furniture', function(req, res, next) {
-  res.render('furniture', { title: 'Search Results - Furniture ' });
+router.get('/', function(req, res, next) {
+  res.render('furniture', { title: 'Search Results - Furniture' });
 });
 
 module.exports = router;

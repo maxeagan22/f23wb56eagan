9aa5c862186ae28a64de44e40259c4dd4b745f18
maxeagan22 <124809445+maxeagan22@users.noreply.github.com>
2023-11-01 18:30:29 -0500

    Step 8 complete
    Step 8 Complete

--- routes/board.js
+++ routes/board.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET furniture page. */
+/* GET board page. */
 router.get('/', function(req, res, next) {
   res.render('board', { title: 'Board Display' });
 });
